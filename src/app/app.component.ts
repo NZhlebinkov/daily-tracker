50508caacb879ec5ca1faf0ec4821dce9461493a
Nikko Zhlebinkov <nzhlebinkov@gmail.com>
2020-09-03 11:29:38 +0300

    Set up basic sync with a firebase database
    For now that just shows changes to one value in the database
    and is only in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from "firebase/app";
+import "firebase/database";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'daily-tracker';
+
+  ngOnInit() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyAAUTnEDtQJmWBPCjE_LKE2EoHV3Vdt2No",
+      authDomain: "intro-4b57d.firebaseapp.com",
+      databaseURL: "https://intro-4b57d.firebaseio.com",
+      projectId: "intro-4b57d",
+      storageBucket: "intro-4b57d.appspot.com",
+      messagingSenderId: "859950668676",
+      appId: "1:859950668676:web:a460f43bbc35708cbc9bd1"
+    };
+    firebase.initializeApp(firebaseConfig);
+
+    var database = firebase.database();
+  
+    const preObject = document.getElementById('object');
+    const dbRefObject = database.ref().child('object');
+  
+    dbRefObject.on('value', snap => console.log(snap.val()));
+  }
+  // Your web app's Firebase configuration
+  
 }

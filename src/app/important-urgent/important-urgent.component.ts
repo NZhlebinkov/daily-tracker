d7e1e98f8f361f1368d46eb63f5dbc197877b06f
Nikko Zhlebinkov <nzhlebinkov@gmail.com>
2020-11-01 08:43:18 +0100

    Some thought on the organization of the component

--- src/app/important-urgent/important-urgent.component.ts
+++ src/app/important-urgent/important-urgent.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./important-urgent.component.css']
 })
 export class ImportantUrgentComponent implements OnInit {
+  tasks: string[] // Do I make 4 arrays for the different categories, a 2D array, or sth else?
+  // Will I implement a way to drag tasks from one category to the other, and inside the list?
 
-  constructor() { }
+  constructor() {
+    this.tasks = [];
+   }
 
   ngOnInit(): void {
+    
   }
 
 }

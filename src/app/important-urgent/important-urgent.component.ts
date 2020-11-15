bd2fb0038ab9414ea5c0739ab363ad1170009f90
Nikko Zhlebinkov <nzhlebinkov@gmail.com>
2020-11-15 08:51:53 +0100

    Setup the database refs for iu but haven't tested

--- src/app/important-urgent/important-urgent.component.ts
+++ src/app/important-urgent/important-urgent.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import "firebase/database";
+import { FormArray, FormControl } from '@angular/forms';
+import { database } from 'firebase';
 import { iu_tasks as dbAddress } from '../db-addresses';
 
 @Component({
@@ -9,12 +10,31 @@ import { iu_tasks as dbAddress } from '../db-addresses';
 })
 export class ImportantUrgentComponent implements OnInit {
   dbAddress = dbAddress;
+  iu: database.Reference;
+  niu: database.Reference;
+  inu: database.Reference;
+  ninu: database.Reference;
+  // tasks = {
+  //   "iu": [], // important-urgent
+  //   "niu": [], // not-important-urgent
+  //   "inu": [], // important-not-urgent
+  //   "ninu": [] // not-important-not-urgent
+  // }; // Do I make 4 arrays for the different categories, a 2D array, or sth else?
   tasks = {
-    "iu": [], // important-urgent
-    "niu": [], // not-important-urgent
-    "inu": [], // important-not-urgent
-    "ninu": [] // not-important-not-urgent
-  }; // Do I make 4 arrays for the different categories, a 2D array, or sth else?
+    iu: new FormArray([]),
+    niu: new FormArray([]),
+    inu: new FormArray([]),
+    ninu: new FormArray([])
+  }
+  // tasks = new FormArray({
+  //   iu: new FormArray([]),
+    
+  // }
+  //   new FormControl('iu'),
+  //   new FormControl('niu'),
+  //   new FormControl('inu'),
+  //   new FormControl('ninu')
+  // ]);
   // Will I implement a way to drag tasks from one category to the other, and inside the list?
 
   constructor() { }
@@ -24,6 +44,8 @@ export class ImportantUrgentComponent implements OnInit {
       // Would a two way binding be useful in this case?
       /* Bind to a child task-category component
         Populating the tasks would happen inside the component */
+        console.log(dbAddress[taskType]);
+        this[taskType] = database().ref(dbAddress[taskType]); // TODO
     }
   }
 
